e4176bf35cfdf152a195cbab2d13a0bccc53cbed
Ariel Cerda Hernández <metabolic.ds@gmail.com>
2021-04-28 00:41:40 +1200

    incluyendo Lotties

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+var	animation = bodymovin.loadAnimation({
+	container: document.getElementById('lottie'),
+	renderer: 'svg',
+	loop: false,
+	autoplay: true,
+	path: '/assets/lottie/data.json'
+})
+var	animation = bodymovin.loadAnimation({
+	container: document.getElementById('kiora'),
+	renderer: 'svg',
+	loop: false,
+	autoplay: true,
+	path: '/assets/lottie/ac.json'
+})
+var	animation = bodymovin.loadAnimation({
+	container: document.getElementById('link'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'https://assets9.lottiefiles.com/packages/lf20_ozmeOp/Minimize.json'
+})
+console.log("Hello, World!");
